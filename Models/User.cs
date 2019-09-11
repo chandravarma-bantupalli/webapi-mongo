a60de0fc0a300c880489bdd13dec63a9b13547e2
chandravarma.bantupalli <chandravarma.bantupalli@cgi.com>
2019-09-11 23:02:46 +0530

    Added Demo User Model to the APi

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DemoAPI.Models
+{
+  public class User
+  {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string userId { get; set; }
+    public string userName { get; set; }
+    public string userCity { get; set; }
+    public string userEmail { get; set; }
+    public string userPhone { get; set; }
+  }
+}
